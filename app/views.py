e045da39daf4330317b1618c397c88ea62eaa14e
Agshin Nadirov <85677228+Agshin2004@users.noreply.github.com>
2024-05-03 11:53:11 +0400

    Update views.py

--- app/views.py
+++ app/views.py
@@ -5,17 +5,14 @@ import requests
 
 API_KEY = os.environ['weather_api']
 BASE_URL = 'http://api.weatherapi.com/v1/current.json?key='
-print(API_KEY)
 
 # Create your views here.
 def home(request):
     
     city_from_request = request.GET.get('search-bar') or 'London'
-    print(city_from_request)
     
     response = requests.get(f'{BASE_URL}{API_KEY}&q={city_from_request}')
     json_response = response.json()
-    print(json_response)
     city = json_response['location']['name']
     temp_c = json_response['current']['temp_c']
     condition = json_response['current']['condition']['text']
